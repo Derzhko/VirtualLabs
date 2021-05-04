3124abf34d985ce189f4646bf33091d32697ace8
Derzhko <alxDerzhko@yandex.ru>
2021-05-04 04:14:08 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' } # выдуманный пользователь
+    posts =[
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
