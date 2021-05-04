041abd35ff5da9eb90e69a2b3cd1605e89747ad1
Derzhko <alxDerzhko@yandex.ru>
2021-05-04 04:34:05 +0300

    create button

--- app/views.py
+++ app/views.py
@@ -1,21 +1,13 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
-    posts =[
-        {
-            'author': { 'nickname': 'John' },
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': { 'nickname': 'Susan' },
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-        title = 'Home',
-        user = user,
-        posts = posts)
+#rendering the HTML page which has the button
+@app.route('/json')
+def json():
+    return render_template('json.html')
+
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print ("Hello")
+    return ("nothing")
